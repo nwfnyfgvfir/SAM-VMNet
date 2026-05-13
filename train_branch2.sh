69121d902f830d67caf102c6ada3c41035714452
Your <you@example.cc>
2026-05-13 16:16:18 +0800

    更新branch1模型

--- train_branch2.sh
+++ train_branch2.sh
@@ -15,4 +15,4 @@ CUDA_VISIBLE_DEVICES=0 python train_branch2.py \
     --work_dir "./result_branch2/" \
     --data_path "./data/vessel/" \
     --medsam_path "./pre_trained_weights/medsam_vit_b.pth" \
-    --branch1_model_path "./pre_trained_weights/best-epoch142-loss0.3230.pth"
+    --branch1_model_path "./pre_trained_weights/best-epoch159-loss0.3441.pth"
